557319efa8ce66d56f3d052ec76c206c11596c1b
chibaf <chibaf@mac.com>
2022-10-04 03:22:44 +0900

    Add files via upload

---
+++ setround.c
@@ -0,0 +1,33 @@
+#include <mex.h>
+#include <fenv.h>
+#include <float.h>
+
+#pragma STDC FENV_ACCESS ON
+
+void mexFunction ( int nlhs, mxArray *plhs[],
+                   int nrhs, const mxArray *prhs[] ) {
+
+  int rnd = (int) mxGetScalar (prhs[0]);
+  int mode = FE_TONEAREST;
+
+  switch (rnd)
+    {
+      case -1:
+        mode = FE_DOWNWARD;
+        break;
+      case 0:
+        mode = FE_TONEAREST;
+        break;
+      case 1:
+        mode = FE_UPWARD;
+        break;
+      case 2:
+        mode = FE_TOWARDZERO;
+        break;
+      default:
+        mode = FE_TONEAREST;
+        break;
+    }
+
+  fesetround (mode);
+}
